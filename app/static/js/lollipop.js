94fed4677a2eed5b69ff55e59acb24b8b887d8d9
Bob Leijnse <bobleynse@hotmail.com>
2021-02-15 12:34:20 +0100

    Cleaned lollipop and added css

--- app/static/js/lollipop.js
+++ app/static/js/lollipop.js
@@ -40,12 +40,6 @@ function createNewChart() {
     chart_group.append("g")
         .attr("transform", "translate(" + 0 + "," + chart_height + ")")
         .call(d3.axisBottom(x));
-        // Code for vertical bar chart
-        // .selectAll("text")
-        // .attr("y", 0)
-        // .attr("x", 9)
-        // .attr("transform", "rotate(90)")
-        // .style("text-anchor", "start");
 
     // Make y-axis
     chart_group.append("g")
@@ -57,13 +51,13 @@ function createNewChart() {
     .data(map.entries())
     .enter()
     .append("circle")
-    // .attr("class", "bar")
+    .attr("class", "lollipop")
     .attr("r", 7)
     .attr("cx", function(d) { return x(d.value); })
     .attr("cy", function(d) { return y(d.key); })
 
-    // .attr("height", y.bandwidth())
-    .on("mouseover", function(d, i) {
+    // Show labels when the mouse is on a lolli
+    .on("mouseenter", function(d, i) {
         var x_var = d.key;
         var value = d.value;
         var info = get_info_on_var(x_var);
@@ -72,27 +66,13 @@ function createNewChart() {
 
         displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
             value + "%<br /><b>Explanation: </b>" + definition)
-
-        //d3.select(this).attr("fill", "DarkOrange");
-    })
-    .on("mousemove", function(d, i) {
-        var x_var = d.key;
-        var value = d.value;
-        var info = get_info_on_var(x_var);
-        var label = info[0]
-        var definition = info[1];
-
-        displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
-            value + "%<br /><b>Explanation: </b>" + definition)
-
-        //d3.select(this).attr("fill", "DarkOrange");
     })
     .on("mouseout", function(d) {
         hideTooltip();
-        //d3.select(this).attr("fill", "steelblue");
+        d3.select(this).attr("fill", "steelblue");
     });
 
-    // text label for the x axis
+    // Text label for the x axis
     svgContainer.append("text")             
     .attr("transform",
             "translate(" + (width/2 - (100/2)) + " ," + 
@@ -101,6 +81,7 @@ function createNewChart() {
     .style("font-size", "13px")
     .text("Percentage");
 
+    // Text label for the title
     chart_group.append("text")
             .attr("class", "title")
             .attr("id", "chart-title")
@@ -109,97 +90,4 @@ function createNewChart() {
             .style("font-weight", "bold")               
             .style("text-anchor", "middle")
             .text("Rental statistics of " + selected_area);
-
-    // Code for vertical bar chart
-    // chart_group.selectAll(".bar")
-    //     .data(map.entries())
-    //     .enter()
-    //     .append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", function (d) { return x(d.key)})
-    //     .attr("y", function (d) { return y(d.value) })
-    //     .attr("width", x.bandwidth())
-    //     .attr("height", function(d) { return chart_height - y(d.value); })
-    //     .on("mouseover", function(d, i) {
-    //         var x_var = d.key;
-    //         var value = d.value;
-    //         var info = get_info_on_var(x_var);
-    //         var label = info[0]
-    //         var definition = info[1];
-
-    //         displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
-    //             value + "%<br /><b>Explanation: </b>" + definition)
-
-    //         //d3.select(this).attr("fill", "DarkOrange");
-    //     })
-    //     .on("mousemove", function(d, i) {
-    //         var x_var = d.key;
-    //         var value = d.value;
-    //         var info = get_info_on_var(x_var);
-    //         var label = info[0]
-    //         var definition = info[1];
-
-    //         displayTooltip("<b>Variable: </b>" + label + "<br /><b>Percentage: </b>" + 
-    //             value + "%<br /><b>Explanation: </b>" + definition)
-
-    //         //d3.select(this).attr("fill", "DarkOrange");
-    //     })
-    //     .on("mouseout", function(d) {
-    //         hideTooltip();
-    //         //d3.select(this).attr("fill", "steelblue");
-    //     });
-
 };
-
-
-
-/////////////////////////////////////////////////////////////////
-//////// EXAMPLE CODE FOR DIFFERENT SHAPE ELEMENTS IN D3 ////////
-
-// var circle = svgContainer.append("circle")
-// 						.attr("cx", 30)
-// 						.attr("cy", 30)
-// 						.attr("r", 20);
-
-//  var rectangle = svgContainer.append("rect")
-// 							.attr("x", 10)
-// 							.attr("y", 10)
-// 							.attr("width", 50)
-// 							.attr("height", 100);
-
-
-// var ellipse = svgContainer.append("ellipse")
-// 							.attr("cx", 50)
-// 							.attr("cy", 50)
-// 							.attr("rx", 25)
-// 							.attr("ry", 10)
-// 							.attr("fill", "red")
-// 							.attr("id", "ellipse");
-
-// d3.select("#ellipse").attr("fill","green");
-
-
-// var line = svgContainer.append("line")
-//                          .attr("x1", 5)
-//                          .attr("y1", 5)
-//                          .attr("x2", 50)
-//                          .attr("y2", 50)
-//                          .attr("stroke-width", 2)
-//                          .attr("stroke", "blue");
-
-// var arc = d3.arc()
-//     .innerRadius(40)
-//     .outerRadius(100)
-//     .startAngle(0)
-//     .endAngle(3);
-
-// svgContainer.append("path")
-// 	.attr("transform", "translate(" + 100 + "," + 100 + ")")
-//     .attr("d", arc)
-//     .attr("fill", "red")
-//     .attr("class", "arc")
-//     .on("click", function(d) {
-//     	d3.select(".arc").attr("fill","blue");
-//     });
-
-/////////////////////////////////////////////////////////////////
